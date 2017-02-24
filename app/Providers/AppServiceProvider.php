8c1e1df8a4bd11e9d6a38469a20c269f6a9d3ff8
Edwin Heij <e.heij@konnectit.nl>
2017-02-24 19:01:44 +0100

    Start project

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Laravel\Dusk\DuskServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        if ($this->app->environment('local', 'testing')) {
+            $this->app->register(DuskServiceProvider::class);
+        }
+//        if (config('app.debug')) {
+//            Debugbar::enable();
+//        } else {
+//            Debugbar::disable();
+//        }
+    }
+}
